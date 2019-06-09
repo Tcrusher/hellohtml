aa1ae3ee963e74ff808afa8ac9a5c03afc0d95ed
Tcrusher <chaifang0620@163.com>
2019-06-09 11:18:57 +0800

    change file

---
+++ js/login.js
@@ -0,0 +1,25 @@
+$(function() {
+    $("#login").click(function() {
+		login();
+    });
+	return false;
+});
+function login() {
+	$.ajax({
+		url : "http://localhost:8080/login",
+		type : "POST",
+		data : {
+			username : $("#username").val(),
+			password : $("#password").val()
+		},
+		success : function(data) {
+			if (data.code == 200) {
+				window.location.href = "index.html";
+				alert(data.data);
+				alert(data.message);
+			} else {
+				alert("登录失败");
+			}
+		}
+	});
+}
